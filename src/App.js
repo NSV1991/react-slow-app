53530ee7365f0a4c573c404cf027bc8071e7aa3c
NSV1991 <neerajvageele451@gmail.com>
2020-11-10 15:52:48 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./style.css";
-// import DemoComponent from "./DemoComponent";
+import { DemoComponent } from "./DemoComponent";
+import { Example } from "./Example";
 
-const App = () => (
-  <div>
-    hello // <DemoComponent style={{ marginTop: 10 }} />
-  </div>
-);
+const sleep = time => {
+  const done = Date.now() + time;
+  while (done > Date.now()) {
+    // sleep...
+  }
+};
+
+const App = () => {
+  const [value, setValue] = React.useState("");
+  const [time, setTime] = React.useState(200);
+  return (
+    <div>
+      <Example time={time} value={value} sleep={sleep} onChange={setValue} />
+      <DemoComponent time={time} sleep={sleep} onChange={setTime} />
+    </div>
+  );
+};
 
 export default App;
--- src/DemoComponent.js
+++ src/DemoComponent.js
@@ -1,3 +1,16 @@
 import React from "react";
 
-export const DemoComponent = props => <div style={props.style}>Demo</div>;
+export const DemoComponent = ({ sleep, time, onChange }) => {
+  sleep(time);
+  return (
+    <div>
+      Late by{" "}
+      <input
+        value={time}
+        type="number"
+        onChange={e => onChange(Number(e.target.value))}
+      />
+      ms slow
+    </div>
+  );
+};
